92115bdd682a74b98785867efe02dfbb81bd4473
Dimitrii Vinogradov <siteddv@gmail.com>
2021-05-18 02:50:56 +0600

    feature: changed Header type into class component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import style from './Header.module.css'
 
-const Header = () => {
-   return (
-      <header className={style.header}>
-         <div className={style.img}></div>
-      </header>
-   );
+class Header extends React.Component {
+   render() {
+      return (
+         <header className={style.header}>
+            <div className={style.img}></div>
+         </header>
+      );
+   }
+
 }
 
 export default Header;
