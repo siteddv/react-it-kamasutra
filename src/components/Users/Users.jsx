d4e0aac9452eace3c41cbbfc9867e3a46680f4ef
Dimitrii Vinogradov <siteddv@gmail.com>
2021-05-17 17:52:43 +0600

    feature: started working with axios requests

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,24 +1,36 @@
 import React from 'react';
 import style from './Users.module.css';
+import * as axios from 'axios';
 
 
 const Users = (props) => {
+
+   if (props.users.length === 0) {
+      axios.get("https://social-network.samuraijs.com/api/1.0/users")
+         .then(response => {
+            props.setUsers(response.data.items);
+         });
+   }
+
    let usersElements = props.users.map(user => (
       <div className={style.usersElement}>
          <div className={style.avatarFollow}>
             <div className={style.avatar}>
-               <img className={style.avatarImg} src={user.photoUrl} alt={""} />
+               <img className={style.avatarImg}
+                  src={user.photos.small != null ? user.photos.small : "https://webtous.ru/wp-content/uploads/2017/09/round-avatar.png"}
+                  alt={""}
+               />
             </div>
             <button className={`${style.button} ${style.followButton}`} onClick={() => { props.followUnfollowUser(user.id) }}>{user.followed ? "Follow" : "Unfollow"}</button>
          </div>
          <div className={style.info}>
             <div className={style.info__about}>
-               <div className={style.fullName}>{user.fullName}</div>
+               <div className={style.fullName}>{user.name}</div>
                <div className={style.status}>{user.status}</div>
             </div>
             <div className={style.info__location}>
-               <div className={style.country}>{user.location.country}</div>
-               <div className={style.city}>{user.location.city}</div>
+               <div className={style.country}>Kyrgyzstan</div>
+               <div className={style.city}>Bishkek</div>
             </div>
          </div>
       </div>
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -3,9 +3,7 @@ const SET_USERS = "SET-USERS"
 
 let initialState = {
    users: [
-      { id: 1, photoUrl: "https://webtous.ru/wp-content/uploads/2017/09/round-avatar.png", followed: false, fullName: "Dimitrii", status: "I'm a programmer", location: { city: "Bishkek", country: "Kyrgyzstan" } },
-      { id: 2, photoUrl: "https://webtous.ru/wp-content/uploads/2017/09/round-avatar.png", followed: false, fullName: "Nastya", status: "I'm a girl", location: { city: "Prigorodnoe", country: "Russia" } },
-      { id: 3, photoUrl: "https://webtous.ru/wp-content/uploads/2017/09/round-avatar.png", followed: true, fullName: "Meerim", status: "I'm Meerim", location: { city: "KRSU", country: "Kyrgyzstan" } },
+
    ]
 };
 
