22477fd1589fedba6171a7a0517be1728e3440d2
Dimitrii Vinogradov <siteddv@gmail.com>
2021-05-18 02:53:47 +0600

    feature: changed Navbar type into class component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,29 +2,32 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import style from './Navbar.module.css'
 
-const Navbar = () => {
-   return (
-      <div className={style.nav}>
-         <div className={style.item}>
-            <NavLink to="/profile" activeClassName={style.active}>Profile</NavLink>
+class Navbar extends React.Component {
+
+   render() {
+      return (
+         <div className={style.nav}>
+            <div className={style.item}>
+               <NavLink to="/profile" activeClassName={style.active}>Profile</NavLink>
+            </div>
+            <div className={style.item}>
+               <NavLink to="/messages" activeClassName={style.active}>Messages</NavLink>
+            </div>
+            <div className={style.item}>
+               <NavLink to="/users" activeClassName={style.active}>Users</NavLink>
+            </div>
+            <div className={style.item}>
+               <NavLink to="/news" activeClassName={style.active}>News</NavLink>
+            </div>
+            <div className={style.item}>
+               <NavLink to="/music" activeClassName={style.active}>Music</NavLink>
+            </div>
+            <div className={style.item}>
+               <NavLink to="/settings" activeClassName={style.active}>Settings</NavLink>
+            </div>
          </div>
-         <div className={style.item}>
-            <NavLink to="/messages" activeClassName={style.active}>Messages</NavLink>
-         </div>
-         <div className={style.item}>
-            <NavLink to="/users" activeClassName={style.active}>Users</NavLink>
-         </div>
-         <div className={style.item}>
-            <NavLink to="/news" activeClassName={style.active}>News</NavLink>
-         </div>
-         <div className={style.item}>
-            <NavLink to="/music" activeClassName={style.active}>Music</NavLink>
-         </div>
-         <div className={style.item}>
-            <NavLink to="/settings" activeClassName={style.active}>Settings</NavLink>
-         </div>
-      </div>
-   );
+      );
+   }
 }
 
 export default Navbar;
